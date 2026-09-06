3f7aebe334b6a6703f1c8a629561a04d762c71c5
Ronakagarwal01 <ronakagarwaloffical@gmail.com>
2026-09-06 17:07:06 +0530

    fixong about us page

--- worker.js
+++ worker.js
@@ -1,5 +1,19 @@
 export default {
   async fetch(request, env) {
+    const url = new URL(request.url);
+    const cleanRoutes = {
+      "/about": "/about.html",
+      "/programs": "/programs.html",
+      "/pitch-day": "/pitch-day.html",
+      "/insights": "/insights.html",
+      "/contact": "/contact.html",
+    };
+
+    if (cleanRoutes[url.pathname]) {
+      url.pathname = cleanRoutes[url.pathname];
+      return env.ASSETS.fetch(new Request(url, request));
+    }
+
     return env.ASSETS.fetch(request);
   },
 };
